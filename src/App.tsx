7e155749ab123c1fd68d13b530406b5a09920b4c
Philip Lee <philipblee@gmail.com>
2026-04-03 18:36:18 -0400

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 import PinLock from './pages/PinLock';
 import Dashboard from './pages/Dashboard';
